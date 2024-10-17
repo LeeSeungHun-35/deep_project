9814950a8edc40ea6e0b5d59308a57045326a803
LeeSeungHun-35 <ish0542@naver.com>
2024-10-17 13:13:56 +0900

    Edit

--- Backend/Server.js
+++ Backend/Server.js
@@ -6,12 +6,12 @@ const PORT = 3001;
 
 app.use(express.json());              
 
-// GET 요청 처리
+// GET 요청 
 app.get('/', (req, res) => {
     res.send('백엔드 node 테스트');     
 });
 
-// POST 요청 처리
+// POST 요청 
 app.post('/data', (req, res) => {
     console.log(req.body);           
     res.json({                      
@@ -20,7 +20,6 @@ app.post('/data', (req, res) => {
     });
 }); 
 
-// 포트 지정
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
